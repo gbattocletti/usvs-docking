e41036dcfcd1fca9465771b13b38402d063b6b89
gbattocletti <g.battocletti@tudelft.nl>
2025-06-05 13:21:53 +0200

    Limit test with high sideways current

--- scripts/sim_sl_mpc.py
+++ scripts/sim_sl_mpc.py
@@ -21,7 +21,7 @@ os.chdir(script_dir)
 
 # Simulation parameters
 sim_t = 0.0  # simulation time [s]
-sim_t_end = 100.0  # simulation duration [s]
+sim_t_end = 60.0  # simulation duration [s]
 sim_dt = 0.001  # simulation time step [s]
 sim_n = int(sim_t_end / sim_dt)  # number of time steps []
 t_vec = sim_dt * np.arange(sim_n)  # time vector [s]
@@ -39,10 +39,14 @@ plant.set_initial_conditions(np.zeros(6))
 
 # Initialize disturbances
 dist = disturbances.Disturbances()
-v_curr = np.zeros(3)  # current speed (stationary, measured) # [TEMP]
-v_wind = np.zeros(3)  # wind speed (stationary, measured) # [TEMP]
-b_curr = np.zeros(6)  # current force # [TEMP] --> get from dist.current()
-b_wind = np.zeros(6)  # wind force # [TEMP] --> get from dist.wind()
+dist.set_current_direction(-np.pi / 4)  # [rad]
+dist.set_current_speed(0.5)  # [m/s]
+dist.set_wind_direction(0.0)  # [rad]
+dist.set_wind_speed(0.0)  # [m/s]
+v_curr = dist.current()  # (3, ) current speed in inertial frame
+v_wind = dist.wind()  # (3, ) wind speed in inertial frame
+b_curr = plant.crossflow_drag(v_curr)  # (3, ) current force in body frame
+b_wind = plant.wind_load(v_wind)  # (3, ) wind force in body frame
 
 # Initialize MPC controller
 mpc = linear_mpc.Mpc()
@@ -75,9 +79,9 @@ mpc.init_ocp()
 
 # Initialize variables
 q_ref = np.zeros(6)  # state reference
-q_ref[0] = 12.0  # [m]
-q_ref[1] = 3.0  # [m]
-q_ref[2] = np.pi / 6  # [rad]
+q_ref[0] = -6.0  # [m]
+q_ref[1] = 0.0  # [m]
+q_ref[2] = 0.0  # [rad]
 w_q = np.zeros(6)  # measurement noise
 w_u = np.zeros(4)  # actuation noise
 q_meas = np.zeros(6)  # measured state
@@ -113,7 +117,7 @@ for i in range(sim_n):
         if VERBOSE and i != 0:
             i_prev = max(0, i - ctrl_n)
             print(
-                f"\nt = {i_prev+1}/{sim_n} [{(i_prev+1) / sim_n * 100:.4f}%]:"
+                f"\nt = {i}/{sim_n} [{i / sim_n * 100:.4f}%]:"
                 f"\n\tq plant =\t[{q0[0]:.4f}, {q0[1]:.4f}, {q0[2]:.4f}, "
                 f"{q0[3]:.4f}, {q0[4]:.4f}, {q0[5]:.4f}]"
                 f"\n\tq+ plant =\t[{plant.q[0]:.4f}, {plant.q[1]:.4f}, "
@@ -129,8 +133,8 @@ for i in range(sim_n):
                 f"{q_pred[5, 1]:.4f}]"
                 f"\n\tu =\t\t[{u[0]:.4f}, {u[1]:.4f}, {u[2]:.4f}, {u[3]:.4f}]"
                 f"\n\tw_u =\t\t[{w_u[0]:.4f}, {w_u[1]:.4f}, {w_u[2]:.4f}, {w_u[3]:.4f}]"
-                f"\n\tb_curr =\t[{b_curr[3]:.4f}, {b_curr[4]:.4f}, {b_curr[5]:.4f}]"
-                f"\n\tb_wind =\t[{b_wind[3]:.4f}, {b_wind[4]:.4f}, {b_wind[5]:.4f}]"
+                f"\n\tb_curr =\t[{b_curr[0]:.4f}, {b_curr[1]:.4f}, {b_curr[2]:.4f}]"
+                f"\n\tb_wind =\t[{b_wind[0]:.4f}, {b_wind[1]:.4f}, {b_wind[2]:.4f}]"
                 f"\n\tcost mpc =\t{cost:.4f}"
             )
 
@@ -138,6 +142,8 @@ for i in range(sim_n):
         w_q = dist.measurement_noise()  # generate measurement noise
         q_meas = plant.q + w_q  # measure the state (with noise)
         mpc.update_model(q_meas[2])  # linearize the model around current heading
+        b_curr = plant.crossflow_drag(v_curr)  # (3, ) measure current force in body RF
+        b_wind = plant.wind_load(v_wind)  # (3, ) measure wind force in body RF
         u_vec, q_pred, cost = mpc.solve(q_meas, q_ref, b_curr, b_wind)  # solve mpc
         u = u_vec[:, 0]  # extract the first control input from the solution
         w_u = dist.actuation_noise()  # generate actuation noise
@@ -171,13 +177,13 @@ print("\nSimulation completed.")
 # Plot the simulation data
 if SHOW_PLOTS:
     plot_functions.plot_variables(t_vec, u_mat, q_mat[:, :-1], q_ref_mat, cost_mat)
-    plot_functions.phase_plot(q_mat[:, :-1])
+    plot_functions.phase_plot(q_mat[:, :-1], v_curr, v_wind)
     plt.show()
 
 # Save the simulation data
 if SAVE_DATA:
     io.save_sim_data(
-        params, dist, t_vec, q_mat, w_mat, q_meas_mat, u_mat, b_curr, b_wind
+        params, dist, t_vec, q_ref_mat, q_mat, w_mat, q_meas_mat, u_mat, b_curr, b_wind
     )
     print("Simulation data saved.")
 #
