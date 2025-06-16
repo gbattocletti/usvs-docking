2201f33b79e6bc1bcbeeaa68439bc2024af6e7d2
gbattocletti <g.battocletti@tudelft.nl>
2025-06-16 10:55:19 +0200

    Unify simulation scripts

--- scripts/sim_mpc.py
+++ scripts/sim_mpc.py
@@ -1,109 +1,105 @@
+# import datetime
 import datetime
 import os
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.linalg
 
-from seacat_dp.control import linear_mpc
+from seacat_dp.control import linear_mpc, nonlinear_mpc
 from seacat_dp.model import disturbances, nonlinear_model, parameters
-from seacat_dp.utils import io
+from seacat_dp.utils import io, settings
 from seacat_dp.visualization import plot_functions
 
-# Simulations settings
-VERBOSE = False
-SAVE_PLOTS = True
-SHOW_PLOTS = True
-SAVE_ANIM = True
+# Load simulation settings
+sim_settings = settings.SimSettings()
+
+# CHANGE SETTINGS HERE
+sim_settings.q_ref = np.array(
+    [
+        10.0,  # x position [m]
+        -6.0,  # y position [m]
+        -np.pi,  # yaw angle [rad]
+        0.0,  # x velocity [m/s]
+        0.0,  # y velocity [m/s]
+        0.0,  # yaw rate [rad/s]
+    ]
+)
+
+# Print some info to the console
+print(
+    f"Simulation settings:\n"
+    f"\tController: {sim_settings.controller} "
+    f"(ctrl_dt: {sim_settings.ctrl_dt:.4f}, "
+    f"ctrl_N: {sim_settings.ctrl_N})\n"
+    f"\tInitial state: {sim_settings.q_0}\n"
+    f"\tReference state: {sim_settings.q_ref}\n"
+)
+
+########################################################################################
 
 # Set cwd to the script directory
 script_dir = Path(__file__).parent
 os.chdir(script_dir)
 
 # Seed
-np.random.seed(1)  # for reproducibility
+np.random.seed(sim_settings.seed)  # for reproducibility
 
 # Simulation parameters
 sim_t = 0.0  # simulation time [s]
-sim_t_end = 100.0  # simulation duration [s]
-sim_dt = 0.001  # simulation time step [s]
+sim_dt = sim_settings.sim_dt  # simulation time step [s]
+sim_t_end = sim_settings.sim_t_end  # simulation duration [s]
 sim_n = int(sim_t_end / sim_dt)  # number of time steps []
 t_vec = sim_dt * np.arange(sim_n)  # time vector [s]
 ctrl_t = 0.0  # time from the last control input [s] (used to trigger control).
-ctrl_dt = 0.25  # control time step [s]
+ctrl_dt = sim_settings.ctrl_dt  # control time step [s]
 ctrl_n = int(ctrl_dt / sim_dt)  # time steps per control step
-ctrl_N = 20  # prediction horizon
+ctrl_N = sim_settings.ctrl_N  # prediction horizon [steps]
+controller = sim_settings.controller  # controller type
 
 # Initialize model
 params = parameters.Parameters()
 plant = nonlinear_model.NonlinearModel(params)
 plant.set_time_step(sim_dt)
 plant.set_integration_method("rk4")
-plant.set_initial_conditions(np.zeros(6))
+plant.set_initial_conditions(sim_settings.q_0)
 
 # Initialize disturbances
 dist = disturbances.Disturbances()
-dist.set_current_direction(0.0)  # [rad]
-dist.set_current_speed(0.0)  # [m/s]
-dist.set_wind_direction(0.0)  # [rad]
-dist.set_wind_speed(0.0)  # [m/s]
+dist.set_current_direction(sim_settings.h_curr)  # [rad]
+dist.set_current_speed(sim_settings.v_curr)  # [m/s]
+dist.set_wind_direction(sim_settings.h_wind)  # [rad]
+dist.set_wind_speed(sim_settings.v_wind)  # [m/s]
 v_curr = dist.current()  # (3, ) current speed in inertial frame
 v_wind = dist.wind()  # (3, ) wind speed in inertial frame
 b_curr = plant.crossflow_drag(v_curr)  # (3, ) current force in body frame
 b_wind = plant.wind_load(v_wind)  # (3, ) wind force in body frame
 
 # Initialize MPC controller
-mpc = linear_mpc.LinearMpc()
+if controller == "linear_mpc":
+    mpc = linear_mpc.LinearMpc()
+elif controller == "nonlinear_mpc":
+    mpc = nonlinear_mpc.NonlinearMpc()
+elif controller == "pid":
+    raise NotImplementedError("PID controller is not implemented in this script.")
+else:
+    raise ValueError(f"Unknown controller type: {controller}.")
+
 mpc.set_dt(ctrl_dt)
 mpc.set_horizon(ctrl_N)
-mpc.set_discretization_method("zoh")
-mpc.set_model(plant.M_inv, plant.D_L, plant.T, phi=plant.q[2])
-Q = scipy.linalg.block_diag(10e3 * np.eye(2), 10e0, np.eye(2), 0.01)  # pos, vel
-R = scipy.linalg.block_diag(10e-3 * np.eye(2), 10e-1 * np.eye(2))  # stern, bow
-P = Q
+mpc.set_discretization_method(sim_settings.discretization)
+mpc.set_model(plant.M_inv, plant.D_L, plant.T)
+Q = sim_settings.Q
+R = sim_settings.R
+P = sim_settings.P
 mpc.set_weights(Q, R, P)
-u_max = np.array(
-    [
-        params.max_stern_thrust_forward,
-        params.max_stern_thrust_forward,
-        params.max_bow_thrust_forward,
-        params.max_bow_thrust_forward,
-    ]
-)
-u_min = np.array(
-    [
-        params.max_stern_thrust_backward,
-        params.max_stern_thrust_backward,
-        params.max_bow_thrust_backward,
-        params.max_bow_thrust_backward,
-    ]
-)
-mpc.set_input_bounds(u_min, u_max)
-delta_u_max = np.array(
-    [
-        params.max_stern_thrust_forward / params.t_stern * ctrl_dt,
-        params.max_stern_thrust_forward / params.t_stern * ctrl_dt,
-        params.max_bow_thrust_forward / params.t_bow * ctrl_dt,
-        params.max_bow_thrust_forward / params.t_bow * ctrl_dt,
-    ]
-)  # [N/s]
-delta_u_min = np.array(
-    [
-        params.max_stern_thrust_backward / params.t_stern * ctrl_dt,
-        params.max_stern_thrust_backward / params.t_stern * ctrl_dt,
-        params.max_bow_thrust_backward / params.t_bow * ctrl_dt,
-        params.max_bow_thrust_backward / params.t_bow * ctrl_dt,
-    ]
-)  # [N/s]
-mpc.set_input_rate_bounds(delta_u_min / 5, delta_u_max / 5)
+mpc.set_input_bounds(sim_settings.u_min, sim_settings.u_max)
+
+mpc.set_input_rate_bounds(sim_settings.delta_u_min / 5, sim_settings.delta_u_max / 5)
 mpc.init_ocp()
 
 # Initialize variables
-q_ref = np.zeros(6)  # state reference
-q_ref[0] = 6.0  # [m]
-q_ref[1] = 2.0  # [m]
-q_ref[2] = -np.pi / 6  # [rad]
+q_ref = sim_settings.q_ref  # state reference
 w_q = np.zeros(6)  # measurement noise
 w_u = np.zeros(4)  # actuation noise
 q_meas = np.zeros(6)  # measured state
@@ -111,7 +107,7 @@ u = np.zeros(4)  # control input
 cost = 0.0  # mpc cost
 
 # Helper variables
-q0 = np.zeros(6)  # copy of plant.q at the start of the MPC control step
+q_0 = np.zeros(6)  # copy of plant.q at the start of the MPC control step
 q_pred = np.zeros((6, mpc.N + 1))  # predicted state (MPC solution)
 
 # Initialize time series
@@ -125,7 +121,6 @@ w_q_mat = np.zeros((6, sim_n))
 w_u_mat = np.zeros((4, sim_n))
 cost_mat = np.zeros(sim_n)
 
-
 # Run the simulation
 print(f"\nSimulation started... [{datetime.datetime.now().strftime('%H:%M:%S')}]")
 for i in range(sim_n):
@@ -137,12 +132,12 @@ for i in range(sim_n):
         # NOTE: to be able to display the updated plant state, the information is
         # printed at the beginning of the next control step, when the plant dynamics
         # has been rolled out for ctrl_n time steps.
-        if VERBOSE and i != 0:
+        if sim_settings.verbose and i != 0:
             i_prev = max(0, i - ctrl_n)
             print(
                 f"\nt = {i}/{sim_n} [{i / sim_n * 100:.4f}%]:"
-                f"\n\tq plant =\t[{q0[0]:.4f}, {q0[1]:.4f}, {q0[2]:.4f}, "
-                f"{q0[3]:.4f}, {q0[4]:.4f}, {q0[5]:.4f}]"
+                f"\n\tq plant =\t[{q_0[0]:.4f}, {q_0[1]:.4f}, {q_0[2]:.4f}, "
+                f"{q_0[3]:.4f}, {q_0[4]:.4f}, {q_0[5]:.4f}]"
                 f"\n\tq+ plant =\t[{plant.q[0]:.4f}, {plant.q[1]:.4f}, "
                 f"{plant.q[2]:.4f}, {plant.q[3]:.4f}, {plant.q[4]:.4f}, "
                 f"{plant.q[5]:.4f}]"
@@ -161,17 +156,20 @@ for i in range(sim_n):
                 f"\n\tcost mpc =\t{cost:.4f}"
             )
 
-        q0 = plant.q  # save the current state (used for debug when VERBOSE)
+        q_0 = plant.q  # save the current state (used for debug when VERBOSE)
         w_q = dist.measurement_noise()  # generate measurement noise
         q_meas = plant.q + w_q  # measure the state (with noise)
-        mpc.update_model(q_meas[2])  # linearize the model around current heading
         b_curr = plant.crossflow_drag(v_curr)  # (3, ) measure current force in body RF
         b_wind = plant.wind_load(v_wind)  # (3, ) measure wind force in body RF
+
+        if controller == "linear_mpc":
+            mpc.update_model(q_meas[2])  # linearize the model around current heading
         u_vec, q_pred, cost = mpc.solve(q_meas, q_ref, b_curr, b_wind)  # solve mpc
+
         u = u_vec[:, 0]  # extract the first control input from the solution
         w_u = dist.actuation_noise()  # generate actuation noise
         u = u + w_u  # add actuation noise
-        u = np.clip(u, u_min, u_max)  # enforce input bounds
+        u = np.clip(u, sim_settings.u_min, sim_settings.u_max)  # enforce input bounds
         ctrl_t = 0.0  # reset the elapsed time
 
     # perform one plant time step
@@ -190,7 +188,7 @@ for i in range(sim_n):
     ctrl_t += sim_dt
 
     # print progress
-    if not VERBOSE:
+    if not sim_settings.verbose:
         print(
             f"Simulation progress: {i+1}/{sim_n} [{(i+1) / sim_n * 100:.4f}%]", end="\r"
         )
@@ -203,6 +201,8 @@ sim_name, _ = io.generate_filename()
 # Save simulation data
 io.save_sim_data(
     sim_name,
+    params,
+    sim_settings,
     plant,
     mpc,
     dist,
@@ -221,25 +221,40 @@ io.save_sim_data(
 )
 
 # Generate and save plots
-fig_variables = None
+fig_variables = None  # Initialize to None to avoid errors
 fig_phase = None
-if SAVE_PLOTS is True or SHOW_PLOTS is True:
-    fig_variables, _ = plot_functions.plot_variables(
-        t_vec, u_mat, q_mat[:, :-1], q_ref_mat, cost_mat
-    )
-    fig_phase, _ = plot_functions.phase_plot(q_mat[:, :-1], v_curr, v_wind)
 
-if SAVE_PLOTS is True:
+if sim_settings.save_plots is True or sim_settings.show_plots is True:
+    fig_variables, _ = plot_functions.plot_variables(
+        t_vec,
+        u_mat,
+        q_mat[:, :-1],
+        q_ref_mat,
+        cost_mat,
+    )
+    fig_phase, _ = plot_functions.phase_plot(
+        q_mat[:, :-1],
+        v_curr,
+        v_wind,
+    )
+
+if sim_settings.save_plots is True:
     io.save_figure(fig_variables, sim_name, "variables")
     io.save_figure(fig_phase, sim_name, "phase-plot")
 
+if sim_settings.show_plots is True:
+    plt.show(block=False)
+
 # Generate and save animation
-if SAVE_ANIM is True:
+if sim_settings.save_anim is True:
     speed_up_factor = 500
     anim = plot_functions.generate_animation(
-        t_vec, q_mat[:, :-1], q_ref_mat, u_mat, v_curr, v_wind, speed_up_factor
+        t_vec,
+        q_mat[:, :-1],
+        q_ref_mat,
+        u_mat,
+        v_curr,
+        v_wind,
+        speed_up_factor,
     )
     io.save_animation(anim, sim_name)
-
-if SHOW_PLOTS is True:
-    plt.show()
