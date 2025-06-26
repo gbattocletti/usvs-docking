Fix heading error computation

src/seacat_dp/control/linear_mpc.py
@@ -290,13 +290,18 @@ class LinearMpc(Mpc):
         self.b_wind = cp.Parameter((self.n_q,), value=np.zeros(self.n_q))
 
         # Cost function
+        # NOTE: in the current implementation, the angle error is computed as the
+        # difference between the predicted angle and the reference angle, without
+        # considering the periodicity of the angle. This should work for the linear case
+        # but should be checked to make sure it's working as expected.
         cost = 0
         for k in range(self.N):
-            cost += cp.quad_form(self.q[:, k] - self.q_ref[:, k], self.Q)  # state cost
+            err = self.q[:, k] - self.q_ref[:, k]
+            cost += cp.quad_form(err, self.Q)  # state cost
             cost += cp.quad_form(self.u[:, k], self.R)  # input cost
-        cost += cp.quad_form(
-            self.q[:, self.N] - self.q_ref[:, self.N], self.P
-        )  # terminal cost
+
+        err = self.q[:, self.N] - self.q_ref[:, self.N]
+        cost += cp.quad_form(err, self.P)  # terminal cost
 
         # Initial condition
         constraints = [self.q[:, 0] == self.q_0]

src/seacat_dp/control/nonlinear_mpc.py
@@ -307,22 +307,20 @@ class NonlinearMpc(Mpc):
         ## Cost function
         # State cost
         for k in range(self.N):
-            self.cost_function += (
-                (self.q[:, k] - self.q_ref[:, k]).T
-                @ self.Q
-                @ (self.q[:, k] - self.q_ref[:, k])
+            err = self.q[:, k] - self.q_ref[:, k]
+            err[2] = (
+                ca.fmod((self.q[2, k] - self.q_ref[2, k] + np.pi), 2 * np.pi) - np.pi
             )
+            self.cost_function += err.T @ self.Q @ err
 
         # Input cost
         for k in range(self.N):
             self.cost_function += self.u[:, k].T @ self.R @ self.u[:, k]
 
         # Terminal cost
-        self.cost_function += (
-            (self.q[:, -1] - self.q_ref[:, -1]).T
-            @ self.P
-            @ (self.q[:, -1] - self.q_ref[:, -1])
-        )
+        err = self.q[:, -1] - self.q_ref[:, -1]
+        err[2] = ca.fmod((self.q[2, -1] - self.q_ref[2, -1] + np.pi), 2 * np.pi) - np.pi
+        self.cost_function += err.T @ self.P @ err
 
         # Define the objective and set the solver options
         self.ocp.minimize(self.cost_function)